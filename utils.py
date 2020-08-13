bfd07ba4ead849855b221e73e4ac7351d6c33d6c
tbsexton <tbsexton@asu.edu>
2020-08-13 14:16:27 -0400

    added new py file (for ref/copy/paste only)

---
+++ utils.py
@@ -0,0 +1,31 @@
+import micropip
+import pprint
+from functools import partial
+
+def get_textstat(stat, text):
+    import textstat
+    return getattr(textstat, stat)(text)
+
+
+def report_stats(text):
+    
+    stat_funcs = [
+        "text_standard",  # aggregate of several reading gradelevel metrics
+        "difficult_words",
+    ]
+    report = dict(zip(
+        stat_funcs,
+        map(get_textstat, stat_funcs)
+    ))
+    pprint(report)
+    return report
+
+from js import value as text 
+report = micropip.install('textstat').then(report_stats(text))
+
+
+
+
+
+
+
