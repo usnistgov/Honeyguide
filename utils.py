766d9c8ce9e9347cf233c0c775a7a668ff47f213
tbsexton <tbsexton@asu.edu>
2020-08-13 14:48:59 -0400

    made py script into cmd tester, fixed functions

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
-import micropip
-import pprint
+#!/usr/bin/env python3
+from functools import partial
+from pprint import pprint
 
-def get_textstat(stat, text):
+def get_textstat(text, stat):
     import textstat
     return getattr(textstat, stat)(text)
 
@@ -14,11 +15,13 @@ def report_stats(text):
     ]
     report = dict(zip(
         stat_funcs,
-        map(get_textstat, stat_funcs)
+        map(partial(get_textstat,text), stat_funcs)
     ))
     pprint(report)
     return report
 
-from js import value as text 
-report = micropip.install('textstat').then(report_stats(text))
+if __name__ == "__main__":
+    import sys
+    text  = sys.argv[1]
+    report_stats(text)
 
